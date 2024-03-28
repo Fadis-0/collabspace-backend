16fe94ca08ca52ede7fa9deed297f02804d0bbb9
Fadis-0 <ayadfadi10@gmail.com>
2024-03-28 17:07:51 +0000

    First commit

---
+++ src/main/java/dev/demo/oauth2/controller/HomeController.java
@@ -0,0 +1,20 @@
+package dev.demo.oauth2.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello, Home!";
+	}
+
+	@GetMapping("/secured")
+	public String secured() {
+		return "Hello, Secured!";
+	}
+
+}
